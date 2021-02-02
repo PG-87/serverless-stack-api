631d3cdfd7f761f1dadebbb89f086a81f64ecf21
Patrik Gustafsson <54358596+PG-87@users.noreply.github.com>
2021-02-02 10:32:45 +0100

    CRUD added and now work as intended

--- src/methods/CrossCall.java
+++
@@ -1,9 +0,0 @@
-package methods;
-
-public class CrossCall {
-    public static void main(String[] args) {
-
-        System.out.println( MethodsExample.calculateTax(100000) );
-        //MethodsExample.add(1, 1);
-    }
-}
